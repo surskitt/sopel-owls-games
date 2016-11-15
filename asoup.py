74ce5bbb0044decaf7c82fc5e5b0f37fa8319107
sharktamer <donohoe.shane@gmail.com>
2016-11-15 02:09:12 +0000

    First working complete version

--- asoup.py
+++ asoup.py
@@ -4,6 +4,7 @@ from sopel.module import commands, require_privmsg
 from time import sleep
 from string import ascii_uppercase as abc
 from random import sample, choice
+from collections import Counter
 
 
 def setup(bot):
@@ -21,7 +22,8 @@ def start_game(bot, trigger):
     asoup['active'] = True
     asoup['chan'] = trigger.sender
     asoup['scores'] = {}
-    asoup['submissions'] = []
+    asoup['submissions'] = {}
+    asoup['votes'] = {}
     bot.say('asoup started')
 
     asoup['round'] = 1
@@ -30,7 +32,7 @@ def start_game(bot, trigger):
     bot.say('Acro: {}'.format(asoup['acro']))
     bot.say('Send your acros!')
     bot.say('(e.g. /msg {} .asoupmit poo bum tits)'.format(bot.nick))
-    sleep(30)
+    sleep(20)
 
     asoup['round'] = 2
     if not asoup['submissions']:
@@ -39,21 +41,21 @@ def start_game(bot, trigger):
         return
     bot.say('Submission period over! Choose your winner!')
     bot.say('e.g. /msg {} .asoupmit 1'.format(bot.nick))
+    asoup['submissions'] = [{i: asoup['submissions'][i]} for i in asoup['submissions']]
     for n, i in enumerate(asoup['submissions'], start=1):
-        bot.say('{}: {}'.format(n, i))
+        bot.say('{}: {}'.format(n, list(i.values())[0]))
     sleep(10)
 
-    winners = [asoup['submissions'][int(i) - 1]
-               for i in asoup['scores']
-               if asoup['scores'][i] == max(asoup['scores'].values())]
-
-    bot.say('Submission period over!')
+    bot.say('Voting period over!')
+    c = Counter(asoup['votes'].values())
+    winners = [asoup['submissions'][int(i[0])-1] for i in c.items()
+               if i[1] == max(c.values())]
     if len(winners) == 1:
         bot.say('The winner is:')
     else:
         bot.say('The winners are:')
     for i in winners:
-        bot.say(i)
+        bot.say('{1} ({0})'.format(*list(i.items())[0]))
 
     asoup['active'] = False
 
@@ -73,9 +75,9 @@ def asoupmit(bot, trigger):
         if ''.join(i[0] for i in msg.split()).upper() != asoup['acro']:
             bot.say('This doesn\'t match the acro')
             return
-        asoup['submissions'].append(msg)
+        asoup['submissions'][trigger.nick] = msg
     else:
         if not msg.isdigit() and int(msg) > len(asoup['submissions']):
             bot.say('Vote by sending the number of the submission')
             return
-        asoup['scores'][msg] = asoup['scores'].get(msg, 0) + 1
+        asoup['votes'][trigger.nick] = msg
