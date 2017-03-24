ae3a0b9bc3ab050cbd57805f78a2ec60f8549141
sharktamer <donohoe.shane@gmail.com>
2017-03-24 18:02:27 +0000

    fixes

--- gofish.py
+++ gofish.py
@@ -12,14 +12,13 @@ class Player():
         self.pairs = 0
 
     def fish(self, card):
+        if card == 0:
+            return
         if card not in self.hand:
             self.hand.append(card)
         else:
             self.pairs += 1
             self.hand.remove(card)
-
-    def get_hand(self):
-        return sorted(self.hand)
 
     def fished(self, card):
         if card in self.hand:
@@ -27,6 +26,24 @@ class Player():
             return card
         else:
             return 0
+
+    def get_hand(self):
+        return sorted(self.hand)
+
+
+class Deck():
+    def __init__(self):
+        self.cards = shuffle('A23456789XJQK'*4)
+
+    def fish(self):
+        if self.cards:
+            return self.cards.pop()
+        else:
+            return 0
+
+    def isEmpty(self):
+        if not self.cards:
+            return True
 
 
 def setup(bot):
@@ -60,12 +77,12 @@ def start(bot, trigger):
         bot.say('You can\'t play against yourself!')
         return
 
-    bot.memory['gofish']['deck'] = random.sample('A23456789XJQK'*4, 52)
+    bot.memory['gofish']['deck'] = Deck()
 
     # deal cards
     for player in players.values():
         for i in range(7 if len(players) == 2 else 5):
-            player.fish(bot.memory['gofish']['deck'].pop())
+            player.fish(bot.memory['gofish']['deck'].fish())
 
     bot.say('Game started with {}'.format(', '.join(players)))
     bot.memory['gofish']['active'] = True
@@ -83,19 +100,48 @@ def start(bot, trigger):
 
 
 def fish(bot, trigger):
+    fisher = bot.memory['gofish']['current']
+    players = bot.memory['gofish']['players']
+
+    if trigger.nick != fisher.name:
+        return
+
     if not trigger.group(2) or len(trigger.group(2).split()) < 2:
         bot.say('To play: .gofish <player> <card>')
         return
 
     player, card = trigger.group(2).split()
+    card = card.upper()
 
-    if player not in bot.users:
+    if player not in players:
         bot.say('{} isn\'t in the game! Pick again'.format(player))
         return
 
     if card not in 'A23456789XJQK':
         bot.say('{} isn\'t a card! Pick again from A23456789JQK'.format(card))
         return
+
+    if trigger.nick == player:
+        bot.say('Can\'t fish from yourself!')
+        return
+
+    fished = players[player].fished(card)
+
+    if fished:
+        fisher.fish(card)
+        checkwin(bot, trigger)
+        if not bot.memory['gofish']['active']:
+            return
+        bot.say('{} had {}! Go again!'.format(player, card))
+        if len(players[player].hand) == 0:
+            for i in range(5):
+                players[player].fish(bot.memory['gofish']['fish'].fish())
+    else:
+        bot.say('{} didn\'t have {}! Go fish!'.format(player, card))
+        fisher.fish(bot.memory['gofish']['deck'].fish())
+        bot.memory['gofish']['current'] = next(bot.memory['gofish']['tracker'])
+        bot.say('{}\'s turn!'.format(bot.memory['gofish']['current'].name))
+    gofishcards(bot, fisher.name)
 
 
 @commands('gfcards')
@@ -118,8 +164,17 @@ def gofishscores(bot, trigger):
     players = bot.memory['gofish']['players']
 
     bot.say('Current scores:')
-    for player in sorted(players):
-        bot.say('{}: {}'.format(player, players[player].pairs))
+    for player in sorted(players.values(), key=lambda x: x.pairs, reverse=True):
+        bot.say('{}: {}'.format(player.name, player.pairs))
+
+
+def checkwin(bot, trigger):
+    players, deck = bot.memory['gofish']['players'], bot.memory['gofish']['deck']
+    if deck.isEmpty() and not any(len(i.hand() for i in players.values())):
+        gofishend()
+        gofishscores(bot, trigger)
+        winner = max(players.values(), key=lambda x: x.pairs)
+        bot.say('{} is the winner!'.format(winner.name))
 
 
 def shuffle(l):
