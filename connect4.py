2c78cc682d74c51dc3bd0de6d957ffb1d3c999e1
sharktamer <donohoe.shane@gmail.com>
2017-10-05 17:47:31 +0100

    add board object

--- connect4.py
+++ connect4.py
@@ -4,6 +4,23 @@ from sopel.module import commands, rule
 from sopel.formatting import color, colors
 from random import choice
 
+class Board:
+    def __init__(self):
+        self.board = [[0]*7 for i in range(6)]
+
+    def addpiece(self, col_index, player):
+        col = list(zip(*self.board))[col_index]
+
+        row_index = 6 - col[::-1].index(0) - 1
+
+        self.board[row_index][col_index] = player
+
+    def printboard(self):
+        print(self.board)
+        for i in self.board:
+            print ' '.join(str(j) for j in i)
+
+
 def diagonals(L):
     h, w = len(L), len(L[0])
     return [[L[h - p + q - 1][q]
